ca3f2b500e697471a09c32f00fa0a075e1cb2448
vigneshnatarajan <30573329+vigneshnatarajan@users.noreply.github.com>
2022-09-25 02:46:14 +0530

    Update main.tf

--- nlb/main.tf
+++ nlb/main.tf
@@ -1,5 +1,8 @@
 provider "aws" {
   region = "us-east-1"
+  shared_config_files      = ["/home/ec2-user/.aws/config"]
+  shared_credentials_files = ["/home/ec2-user/.aws/credentials"]
+  profile                  = "default"
 }
 
 #locals {
