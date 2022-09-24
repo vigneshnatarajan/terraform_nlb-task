01f51fd90cff379351cda38d81db6e0534cf0fa7
vigneshnatarajan <30573329+vigneshnatarajan@users.noreply.github.com>
2022-09-25 02:11:08 +0530

    Update versions.tf

--- modules/versions.tf
+++ modules/versions.tf
@@ -1,1 +1,14 @@
+terraform {
+  required_version = ">= 1.0.0"
 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.27.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 2.0"
+    }
+  }
+}
