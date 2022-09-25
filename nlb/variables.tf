7f70647ae6b0d75195e0ca761c2532e669a443fc
vigneshnatarajan <30573329+vigneshnatarajan@users.noreply.github.com>
2022-09-25 13:04:50 +0530

    Update variables.tf

--- nlb/variables.tf
+++ nlb/variables.tf
@@ -39,3 +39,31 @@ variable "tls_tg" {
   type        = string
   default     = ""
 }
+
+
+######Ec2-instance Variable##########
+
+variable "key_name" {
+  description = "Name to be used on all the resources as identifier"
+  type        = string
+  default     = ""
+}
+
+variable "instane_type" {
+  description = "Name to be used on all the resources as identifier"
+  type        = string
+  default     = ""
+}
+
+variable "ec2_subnet_id" {
+  description = "Name to be used on all the resources as identifier"
+  type        = string
+  default     = ""
+}
+
+variable "ec2_name" {
+  description = "Name to be used on all the resources as identifier"
+  type        = string
+  default     = ""
+}
+
