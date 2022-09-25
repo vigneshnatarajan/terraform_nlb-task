4ba576c7820a778bad72281b36c96acb352c141c
vigneshnatarajan <30573329+vigneshnatarajan@users.noreply.github.com>
2022-09-25 11:52:37 +0530

    Update variables.tf

--- nlb/variables.tf
+++ nlb/variables.tf
@@ -1,41 +1,41 @@
 variable "nlb_name" {
   description = "Name to be used on all the resources as identifier"
   type        = string
-  default     = "Demo-NLB"
+  default     = ""
 }
 
 variable "vpc_id" {
   description = "Name to be used on all the resources as identifier"
   type        = string
-  default     = "vpc-0e36e57ebebe775b1"
+  default     = ""
 }
 
 variable "subnet_list" {
   description = "Name to be used on all the resources as identifier"
   type        = list(string)
-  default     = ["subnet-0900c416d4f8c2c48","subnet-08288bd327f34a8b5"]
+  default     = ""
 }
 
 variable "tcp_udp_tg" {
   description = "Name to be used on all the resources as identifier"
   type        = string
-  default     = "tcp-udp-tg"
+  default     = ""
 }
 
 variable "udp_tg" {
   description = "Name to be used on all the resources as identifier"
   type        = string
-  default     = "udp-tg"
+  default     = ""
 }
 
 variable "tcp_tg" {
   description = "Name to be used on all the resources as identifier"
   type        = string
-  default     = "tcp-tg"
+  default     = ""
 }
 
 variable "tls_tg" {
   description = "Name to be used on all the resources as identifier"
   type        = string
-  default     = "tls-tg"
+  default     = ""
 }
