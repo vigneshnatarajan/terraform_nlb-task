d624d52db34e763fdfa6e2f90ba054a64dd59b63
vigneshnatarajan <30573329+vigneshnatarajan@users.noreply.github.com>
2022-09-25 13:01:43 +0530

    Create ec2-instance.tf

---
+++ nlb/ec2-instance.tf
@@ -0,0 +1,29 @@
+data "aws_ami" "amazon-linux-2" {
+ most_recent = true
+
+
+ filter {
+   name   = "owner-alias"
+   values = ["amazon"]
+ }
+
+
+ filter {
+   name   = "name"
+   values = ["amzn2-ami-hvm*"]
+ }
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.amazon-linux-2.id
+  instance_type = var.instane_type
+  key_name = var.key_name
+  subnet_id = var.ec2_subnet_id
+  root_block_device {
+   volume_size = 30
+   volume_type = "gp2"
+  }
+  tags = {
+    Name = "demo-tcp-udp-terraform"
+  }
+}
